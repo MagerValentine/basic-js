43a780ffb2a7263f30e760f814288543374a1196
magervalentine <magervalentine@gmail.com>
2020-02-28 19:44:01 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,3 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  
+module.exports = function createDreamTeam(members) {
+  if (members instanceof Array) {
+    let regexp = /([a-z]){1,}/i;
+    members = members.map(m => {
+      if (typeof m == "string") {
+        return m.trim().toUpperCase();
+      }
+      return ' ';
+    }).sort();
+    return members.reduce((accum, current) => {
+      if (current != ' ') return accum + current.trim().toUpperCase()[0];
+      else return accum;
+    }, '');
+  }
+  return '';
 };
