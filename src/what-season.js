0a43d41a608849dce0eb6369bd1194e93f54279d
magervalentine <magervalentine@gmail.com>
2020-02-28 20:17:08 +0300

    feat: implement getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -1,3 +1,15 @@
-module.exports = function getSeason(/* date */) {
+module.exports = function getSeason(date) {
+  if(date == null || date == undefined) return 'Unable to determine the time of year!';
+      if(Object.entries(date).length > 1) throw new Error();
+  
 
+  if(date instanceof Date)
+  {
+    var month = date.getMonth() + 1;
+    if(month < 3 || month == 12) return "winter";
+    if(month < 6 && month > 2) return "spring";
+    if(month > 5 && month < 9) return "summer";
+    if(month > 8 && month < 12) return "fall";
+  }
+  throw new Error();
 };
